9311d4060d629cd5b367bab3e1dc84e927880089
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-13 12:48:47 -0600

    update: set up particles component, set up param const

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 55,
+      desnsity: {
+        enable: true,
+        value_area: 500
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Rank />
         <Logo />
