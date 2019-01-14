5bcb374f6bb77763049bc142e35521a4c1eb7247
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-13 23:19:20 -0600

    update: modified signout functionality

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,35 @@
 import React from "react";
 
-const Navigation = ({ onRouteChange }) => {
-  return (
-    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p
-        onClick={() => onRouteChange("signin")}
-        className="fe link dim black underline pa3 pointer"
-      >
-        {" "}
-        Sign out
-      </p>
-    </nav>
-  );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signout")}
+          className="fe link dim black underline pa3 pointer"
+        >
+          Sign out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="fe link dim black underline pa3 pointer"
+        >
+          Signin
+        </p>
+        <p
+          onClick={() => onRouteChange("register")}
+          className="fe link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Navigation;
