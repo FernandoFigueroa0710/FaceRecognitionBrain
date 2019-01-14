2f8b54bca699613829732f51993cec5d2c6306a3
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-13 18:58:47 -0600

    update: face recognition component set up; image being displayed

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className="center">
-      <img alt="pic" src="https://samples.clarifai.com/face-det.jpg" />
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img alt="pic" src={imageUrl} width="500px" heigh="auto" />
+      </div>
     </div>
   );
 };
