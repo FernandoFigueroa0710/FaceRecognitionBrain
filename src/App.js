3d70eb157b0dec354bea5e58aac717afc4068df6
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-13 21:36:52 -0600

    update: working signin component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
+import Signin from "./components/Signin/Signin";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
@@ -15,10 +16,10 @@ const app = new Clarifai.App({
 const particlesOptions = {
   particles: {
     number: {
-      value: 55,
+      value: 60,
       desnsity: {
         enable: true,
-        value_area: 500
+        value_area: 300
       }
     }
   }
@@ -29,9 +30,29 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {},
+      route: "signin"
     };
   }
+
+  calculateFaceLocation = response => {
+    const clarifaiFace =
+      response.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = response => {
+    this.setState({ box: response });
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -40,35 +61,37 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, //COLOR MODEL API KEY
-        this.state.input
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
       )
-      .then(
-        function(response) {
-          // do something with response
-          console.log(
-            response.outputs[0].data.regions[0].region_info.bounding_box
-          );
-        },
-        function(err) {
-          // there was an error
-        }
-      );
+      .catch(err => console.log("API Error", err));
   };
 
+  onRouteChange = () => {
+    this.setState({ route: "home" });
+  };
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
         <Navigation />
-        <Rank />
-        <Logo />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        {this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        )}
       </div>
     );
   }
