8e3dca93b3b5be4afa62a1f8637fd67a653ffa84
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-15 12:40:15 -0600

    update: all endpoints working

--- API/server/server.js
+++ API/server/server.js
@@ -37,7 +37,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("sucess");
+    res.json(database.users[0]);
   } else {
     res.status(400).json("error loggin in");
   }
@@ -47,9 +47,9 @@ app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
   database.users.push({
     id: "125",
-    name: "Ann",
-    email: "ann@gmail.com",
-    password: "apples",
+    name: name,
+    email: email,
+    password: password,
     entries: 0,
     joined: new Date()
   });
@@ -70,7 +70,7 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
