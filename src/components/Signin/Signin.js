1dd5dcb949bd9e62398838ab91202f73f46b047e
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-15 12:37:35 -0600

    update: chaged the user validation and centered text

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -27,8 +27,9 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(data => {
-        if (data === "sucess") {
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user);
           this.props.onRouteChange("home");
         }
       });
@@ -41,7 +42,7 @@ class Signin extends React.Component {
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <legend className=" center f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">
                   Email
