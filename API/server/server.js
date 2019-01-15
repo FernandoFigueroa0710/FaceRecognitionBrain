cccee4461b5f62abe5b75dd9bfc2eff11c961d12
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-14 22:08:38 -0600

    update: GET request on profile:id working/POST request on image working

--- API/server/server.js
+++ API/server/server.js
@@ -15,7 +15,7 @@ const database = {
       joined: new Date()
     },
     {
-      id: "123",
+      id: "124",
       name: "sally",
       email: "sally@gmail.com",
       password: "bananas",
@@ -51,6 +51,35 @@ app.post("/register", (req, res) => {
     joined: new Date()
   });
   res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found!");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found!");
+  }
 });
 
 app.listen(3001, () => {
