92fd40c131ac6fef33a2cf6e70e9c4bbacaa6086
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-01-14 22:17:57 -0600

    update: deleted API layout

--- API/server/server.js
+++ API/server/server.js
@@ -85,12 +85,3 @@ app.post("/image", (req, res) => {
 app.listen(3001, () => {
   console.log("I am running on port 3001");
 });
-
-/*
-API layout
-/ -> this is working
-/sign in  -> POST = sucess/fail
-/register --> POST = new user
-/profile -> /:userId --> GET = user
-/image --> PUT -> user
- */
