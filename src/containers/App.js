c732b5035d1c237fa11226f0c5028e5abcb99cca
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-05 14:00:15 -0600

    remove the API method and integrate fetch for the new endpoint

--- src/containers/App.js
+++ src/containers/App.js
@@ -78,8 +78,14 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3001/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch("http://localhost:3001/image", {
