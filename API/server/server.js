ac935d6a8d93200725aaa5e626569dae4eaf2b07
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-04 22:59:38 -0600

    create controller / image
    moved the image endpoint to its own controller file

--- API/server/server.js
+++ API/server/server.js
@@ -39,15 +39,7 @@ app.get("/profile/:id", (req, res) => {
 });
 
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json("Unable to get entries"));
+  image.handleImage(req, res, db);
 });
 
 app.listen(3001, () => {
