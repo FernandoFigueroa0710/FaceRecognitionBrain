cf24a656ac18a6a551762ab6fe84f00614577e52
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-05 14:00:42 -0600

    add Calrifai to the image controller

--- API/controllers/image.js
+++ API/controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "f1765d4970dc42c8aa9b95e4cac615eb"
+});
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with Api"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -11,5 +25,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 };
