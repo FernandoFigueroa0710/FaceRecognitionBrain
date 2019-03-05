454371e68a48e2ce7f87806ce45e6cd7495250f7
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-04 20:33:36 -0600

    refactor code move register function on its own controller

--- API/server/server.js
+++ API/server/server.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcrypt");
 const cors = require("cors");
 const knex = require("knex");
 
+const register = require("../controllers/register");
+
 const db = knex({
   client: "pg",
   connection: {
@@ -19,7 +21,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send(db.users);
 });
 
 app.post("/signin", (req, res) => {
@@ -45,32 +47,7 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password, 10);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res;
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("unable to register"));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
